a5a56e1a1aa8c863c8596a692bd3b381d20da784
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-10 22:57:24 +0100

    m

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,14 +4,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
-		putchar('\n');
+		putchar("\n");
 
 return (0);
 }
