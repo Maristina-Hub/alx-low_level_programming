f17a5d85dd7a1c9edc2d60908db3bb4a502ca5a6
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-10 23:23:20 +0100

    m

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Print the alphabet in lowercase letters in reverse
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char c;
 
-	for (c = 'z'; c <= 'a'; c--)
+	for (c = 'z'; c >= 'a'; c--)
 		putchar(c);
 		putchar('\n');
 
 	return (0);
 }
+
+
