dc44b7efdf0ffe3c98904ea7ec3d01f2b7dec22c
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-13 21:57:46 +0100

    m

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,20 @@
-#!/bin/bash
- alias ls="rm *" 
+#include "main.h"
+
+/**
+* main - Print the word "Holberton"
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+	char p[] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
+	return (0);
+}
