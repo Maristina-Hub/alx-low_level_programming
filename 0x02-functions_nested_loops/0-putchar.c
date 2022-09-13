6c2b370b18d04cd12ce98d7d91e487ef95d4a67b
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-13 22:07:21 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* main - Print the word "Holberton"
-*
-* Return: Always 0 (Success)
-*/
+ * main - print Holberton
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	int i;
-	char p[] = "main";
-
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }
