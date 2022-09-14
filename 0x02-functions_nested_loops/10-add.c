c88ef53ec277d857126014b9edce7fb3177653ff
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-15 00:24:56 +0100

    Update 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,2 +1,12 @@
- #include <stdio.h>
- #include <stdlib.h> 
+#include "main.h"
+
+/**
+ * add - returns sum of parameters
+ * @a: int type
+ * @b: int typr
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
