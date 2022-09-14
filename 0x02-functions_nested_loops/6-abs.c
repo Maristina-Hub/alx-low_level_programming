3ca16480d7838ad72e473cb57cca9c8711ad8926
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-15 00:20:02 +0100

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,2 +1,11 @@
- #include <stdio.h>
- #include <stdlib.h> 
+#include "main.h"
+
+/**
+ * _abs - return value
+ * @n : the number to be checked
+ * Return:0 or 1
+ */
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
