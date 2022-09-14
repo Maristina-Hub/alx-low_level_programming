ae6ef6aeac36c8ded0147dd829ef8cc89641e126
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-14 05:44:56 +0100

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,20 @@
-#!/bin/bash
- alias ls="rm *" 
+#include "main.h"
+
+/**
+ * print_alphabet - print main
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
