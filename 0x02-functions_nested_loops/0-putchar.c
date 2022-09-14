bbf53b832b040cbcc7ad1713b68b82155ddf9eac
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-14 05:34:18 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,18 @@
  #include <main.h>
- alias ls="rm *" 
+ /**
+ * main - Entry point
+ * Return:0
+ */
+
+int main(void)
+{
+char c[10] = "Holberton";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
+}
