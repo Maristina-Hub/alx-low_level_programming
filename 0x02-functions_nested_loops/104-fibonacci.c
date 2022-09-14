9fc62f1927b4e6781426ea8438faa01db7336561
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-15 00:46:32 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,23 +2,27 @@
 #include <stdlib.h>
 
 /**
- * main - main block to be executed
- * Description: computes and prints
+ * main - main block
+ * Description: computes and prints the sum of all the multiples of 3 or
+ * 5 below 1024 (excluded), followed by a new line
  * Return: 0
  */
 int main(void)
 {
-	int c = 0;
-	int sum = 0;
+	int i = 0;
+	unsigned long int a = 0, b = 1, next = 0;
 
-	while (c < 1024)
+	while (i < 98)
 	{
-		if (c % 3 == 0 || c % 5 == 0)
-		{
-			sum += c;
-		}
-		c++;
+		next = a + b;
+		a = b;
+		b = next;
+		printf("%lu", next);
+
+		if (i < 97)
+			printf(", ");
+		i++;
 	}
-	printf("%i\n", sum);
+	putchar('\n');
 	return (0);
 }
