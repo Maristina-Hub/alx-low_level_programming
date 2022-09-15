5bcd003c3352a9ea0c9c95a09221adebd562998e
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-15 01:08:44 +0100

    Update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,8 +12,8 @@ int main(void)
 	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
-	unsigned long int aft2; 
-	printf("%lu", bef);
+	unsigned long int aft2 
+		printf("%lu", bef);
 	for (i = 1; i < 91; i++)
 	{	printf(", %lu", aft);
 		aft += bef;
