4141feefe946e5b93f1583bd5f2caa4a4b7766ba
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-15 17:39:57 +0100

    Update 2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,2 +1,11 @@
- #include <unistd.h>
- alias ls="rm *" 
+#include "main.h"
+/**
+ * mul - retuens the results of the parameters
+ * @a: int type number
+ * @b: int type number
+ * Return: 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
