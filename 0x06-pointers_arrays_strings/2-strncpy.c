a8a62492cf2aa68cd6f894d38c381bf4b2172d58
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-21 19:24:00 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,2 +1,21 @@
-#!/bin/bash
-tr 'A-Za-z' 'N-ZA-Mn-za-m' 
+#include "main.h"
+
+/**
+ * *_strncpy - finds the length of the string
+ * @dest: pointer to the string
+ * @src: pointer
+ * @n: int
+ * Return: destination
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for ( ; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
