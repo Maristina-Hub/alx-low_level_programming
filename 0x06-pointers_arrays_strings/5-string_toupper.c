4cb3835d5199ebc0af676a0124b19d8a5f96e6fa
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-21 19:25:30 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,2 +1,19 @@
- #include <unistd.h>
- alias ls="rm *" 
+#include "main.h"
+
+/**
+ * *string_toupper - capitalize string
+ * @str: pointer
+ * Return: capitalizied string
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] <= 'z' && str[i] >= 'a')
+			str[i] -= 32;
+	}
+
+	return (str);
+}
