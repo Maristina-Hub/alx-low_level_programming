9e6d6920d400cc45da7571074056f2e4f0f52012
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-26 20:12:16 +0100

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,2 +1,20 @@
-#!/bin/bash
- printf "%x\n" $DECIMAL
+#include "main.h"
+
+/**
+ * _memcpy - copies n bytes to the pointer of a pointer
+ * @dest: pointer to char parameters
+ * @src: pointer to char params
+ * @n: size
+ * Return: *dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
