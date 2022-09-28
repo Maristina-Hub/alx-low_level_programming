b33beb71b256af8ed035cfe7dff706579466ae9a
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-09-29 00:32:23 +0100

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,2 +1,15 @@
-#!/bin/bash
-tr 'A-Za-z' 'N-ZA-Mn-za-m' 
+#include "main.h"
+/**
+ * _strlen_recursion - size
+ * @s: pointer to string params
+ * Return: recursion
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+
+	return (1 + _strlen_recursion(++s));
+}
