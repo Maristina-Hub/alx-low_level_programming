65bb8ac8c4bac89796d94356a46ce124d1b7692b
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-01 05:41:21 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,2 +1,15 @@
-#!/bin/bash
- printf "%x\n" $DECIMAL
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - A program that prints the number of arguments you passed into it
+ * @argc: An argument counter
+ * @argv: An argument array
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argv[0])
+		printf("%d\n", argc - 1);
+	return (0);
+}
