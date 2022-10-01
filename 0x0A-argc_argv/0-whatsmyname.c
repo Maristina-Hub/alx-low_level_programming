d2c6a1c0b85ae45ac7d9617d7a12d1df84b6d96f
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-01 05:40:42 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,2 +1,12 @@
- #include main.h
- alias ls="rm *" 
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: offset count
+ * @argv: offset value
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
