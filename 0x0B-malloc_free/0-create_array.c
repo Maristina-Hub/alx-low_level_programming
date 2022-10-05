eb9b8ca1f36c0577ea49aed5463c50572fb349ab
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-05 03:21:24 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,16 +6,14 @@
  * Return: a pointer to an array
  */
 char *create_array(unsigned int size, char c)
-{
-	unsigned int i;
-	char *s;
-	if (size == 0)
-		
-	return (NULL);
-	s = (char *)malloc(size * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; i < size; i++)
-		s[i] = c;
-	return (s);
+{unsigned int i;
+char *s;
+if (size == 0)	
+return (NULL);
+s = (char *)malloc(size * sizeof(char));
+if (s == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+s[i] = c;
+return (s);
 }
