153c49ba31cc9ffdf77c48315beca3b975afe3b3
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-05 03:19:34 +0100

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 	char *s;
 	if (size == 0)
-		return (NULL);
+	return (NULL);
 	s = (char *)malloc(size * sizeof(char));
 	if (s == NULL)
 		return (NULL);
