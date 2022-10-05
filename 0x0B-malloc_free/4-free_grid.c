a8a50b93de14cedc5db6dfe4b93ce5f26e8724a3
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-05 03:16:47 +0100

    Update 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,2 +1,14 @@
- #include <unistd.h>
- alias ls="rm *" 
+#include "main.h"
+/**
+ * free_grid - removing memory allocation
+ * @grid: memory block to be freed
+ * @height: height of the array
+ * Return: returns void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+}
