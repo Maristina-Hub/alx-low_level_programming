0bb53fe9384d2df0d87a8cbc2a97466ed50f158c
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-05 03:38:23 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,11 +7,13 @@
 int len(char *str)
 {
 	int len = 0;
+
 	if (str != NULL)
 	{
 		while (str[len])
 			len++;
 	}
+
 	return (len);
 }
 /**
@@ -24,11 +26,14 @@ char *argstostr(int ac, char **av)
 {
 	char *new_string = NULL;
 	int k = 0, i = ac, j, sum = 0, temp = 0;
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
+
 	while (ac--)
 		sum += (len(av[ac]) + 1);
 	new_string = (char *) malloc(sum + 1);
+
 	if (new_string != NULL)
 	{
 		while (k < i)
@@ -45,5 +50,6 @@ char *argstostr(int ac, char **av)
 	{
 		return (NULL);
 	}
+
 	return (new_string);
 }
