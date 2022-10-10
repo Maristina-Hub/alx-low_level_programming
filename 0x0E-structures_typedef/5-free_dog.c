284bf83eaadf1b369d0e3f19426d863e85f20d79
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-10 09:30:59 +0100

    Update 5-free_dog.c

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,2 +1,15 @@
- #include <unistd.h>
- alias ls="rm *" 
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - a function that frees dogs.
+ * @d: pointer to dog struct.
+ * Return: void.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
