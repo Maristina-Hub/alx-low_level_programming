9ff30172118b05e0e4b2844df5701d25ff54aad1
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-10 09:28:55 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,2 +1,18 @@
-#!/bin/bash
-tr 'A-Za-z' 'N-ZA-Mn-za-m' 
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * print_dog - Prints a struct dog
+ * @d: pointer to struct dog
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		d->name == NULL ? printf("(nil)\n") : printf("%s\n", d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: ");
+		d->owner == NULL ? printf("(nil)\n") : printf("%s\n", d->owner);
+	}
+}
