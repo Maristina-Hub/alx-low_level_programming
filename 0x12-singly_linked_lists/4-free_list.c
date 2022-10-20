07b251c7bd5ec26134eaa9f62629113338882509
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-20 23:46:08 +0100

    Update 4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -10,12 +10,12 @@
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
-	}
+list_t *tmp;
+while (head)
+{
+tmp = head->next;
+free(head->str);
+free(head);
+head = tmp;
+}
 }
