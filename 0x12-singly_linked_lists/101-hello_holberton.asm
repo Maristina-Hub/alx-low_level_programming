5a59bc6f84c2fb4f16dd3ee76ff66f9810b0f936
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-10-20 23:34:14 +0100

    Update 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,2 +1,9 @@
- #include <unistd.h>
- alias ls="rm *" 
+ global    main
+          extern    printf
+main:
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
