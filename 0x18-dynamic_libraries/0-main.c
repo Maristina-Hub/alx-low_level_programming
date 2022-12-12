9b0c413c926141b60aee12db7ba7594664a19c1d
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-12-12 20:36:48 +0100

    Update 0-main.c

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,2 +1,12 @@
-#!/usr/bin/python3
- alias ls="rm *" 
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
