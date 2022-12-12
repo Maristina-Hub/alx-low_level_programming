6c8a5f3dc59d7bd2f7d0e0c5e433efa46d3ed681
Maristina <78390336+Maristina-Hub@users.noreply.github.com>
2022-12-12 20:59:02 +0100

    Update 0-main.c

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,2 +1,10 @@
- #include <unistd.h>
- alias ls="rm *" 
+#include <stdio.h>
+#include <stdlib.h>
+#define int char
+int main(void)
+{
+    int i;
+    i = 5;
+    printf ("sizeof(i) = %lu", sizeof(i));
+    return (EXIT_SUCCESS);
+}
